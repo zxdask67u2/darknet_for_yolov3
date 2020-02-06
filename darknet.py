fafa11f40555248f9f411c9912367801d1a2c9fe
zhaoyi <864596494@qq.com>
2020-02-06 17:00:29 +0800

    1

--- darknet.py
+++ darknet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional
+from torch.autograd import Variable
 class con_bn_leaky(nn.Module):
     def __init__(self,in_ch,out_ch,k,stride,padd):
         super(con_bn_leaky, self).__init__()
@@ -11,7 +12,6 @@ class con_bn_leaky(nn.Module):
         ).cuda()
     def forward(self, x):
         x=self.cbl(x)
-        print(x.size())
         return x
 
 class con_bn_leak_5set(nn.Module): #
@@ -51,9 +51,10 @@ class Res_block(nn.Module):
     def forward(self, x):
         return x+self.res(x)
 
-class Main_net(nn.Module):
-    def __init__(self):
-        super(Main_net, self).__init__()
+class draknet53(nn.Module):
+    def __init__(self,class_num):
+        super(draknet53, self).__init__()
+        self.out_filter_ch=3*(class_num+5)
 
         self.d_52=nn.Sequential(
             con_bn_leaky(3,32,3,1,1),#416*416
@@ -168,8 +169,46 @@ class Main_net(nn.Module):
             con_bn_leaky(512, 1024, 3, 1, 1),
             Res_block(1024),
         )
-
+        #13*13输出部分------------------------------------
         self.conv_set_13=con_bn_leak_5set(1024,512)
+        self.out1_conv=nn.Sequential(
+            con_bn_leaky(512,512,3,1,1),
+            nn.Conv2d(512,self.out_filter_ch,1,1,0)
+        )
+
+        #13向26con_set传播部分
+        self.con13_26=nn.Sequential(
+            con_bn_leaky(512,256,1,1,0),
+            upsampling()
+        )    #256*16*16
+
+        #26*26输出部分，------------------------------
+        # 输入：经过concat后（256+512）=768
+        self.conv_set_26=nn.Sequential(
+            con_bn_leak_5set(768,512)
+        )
+        self.out2_conv=nn.Sequential(
+            con_bn_leaky(512,512,3,1,1),
+            nn.Conv2d(512,self.out_filter_ch,1,1,0)
+        )
+
+        #26向52转换部分
+        self.con26_52=nn.Sequential(
+            con_bn_leaky(512,256,1,1,0),
+            upsampling()
+        )
+
+        # 52*52输出部分，------------------------------
+        # 输入：经过concat后（256+256）=512  正合适
+        self.conv_set_52=nn.Sequential(
+            con_bn_leak_5set(512,512)
+        )
+        self.out3_conv=nn.Sequential(
+            con_bn_leaky(512,512,3,1,1),
+            nn.Conv2d(512,self.out_filter_ch,1,1,0)
+        )
+
+
 
 
 
@@ -180,19 +219,42 @@ class Main_net(nn.Module):
 
 
     def forward(self, x):
-        out=self.d_52(x)
-        out=self.d_26(out)
-        out=self.d_13(out)
-        print('输出',out.size())
-        return out
+        x52=self.d_52(x)
+        x26=self.d_26(x52)
+        x13=self.d_13(x26)
+
+        x13_=self.conv_set_13(x13)
+        out1=self.out1_conv(x13_)#13*13输出----------------
+
+        x_13_26=self.con13_26(x13_)
+        x_26_in=torch.cat((x_13_26,x26),dim=1)
+        x_26_=self.conv_set_26(x_26_in)
+        out2=self.out2_conv(x_26_)
+
+        x_52_=self.con26_52(x_26_)
+        x_52_=torch.cat((x_52_,x52),dim=1)
+        out3=self.conv_set_52(x_52_)
+        out3=self.out3_conv(out3)
+
+
+
+        return out1,out2,out3
 
 
 
 if __name__ == '__main__':
+    import time
     device = torch.device("cuda:0")
-    x=torch.rand([2,3,416,416]).to(device)
-    m=Main_net().to(device)
-    out=m(x)
+
+    m=draknet53(1).to(device)
+    m.eval()
+    with torch.no_grad():    #防止累加显存溢出
+        x=torch.rand([2,3,416,416]).to(device)
+        for i in range(3):
+            t1=time.time()
+            out1,out2,out3=m(x)
+            print(time.time()-t1)
+            print(out1.size(),out2.size(),out3.size())
 
 
 
